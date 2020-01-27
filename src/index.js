d91cd01556b35ba4335f37c2248cc2beae1d76e2
arunmohandas <arun.mohandas@highpeaksw.com>
2020-01-27 20:13:35 +0530

    Ticking Clock

--- src/index.js
+++ src/index.js
@@ -1,8 +1,37 @@
-import Reat from 'react';
+import React from 'react';
 import ReatDOM from 'react-dom';
 import './index.css';
 
-ReatDOM.render(
-	'Helo World',
-	document.getElementById('root')
-	);
+class Clock extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {time: new Date()};
+	}
+
+	render() {
+		return (
+			<div>
+				Time now is :  {this.state.time.toLocaleTimeString()}
+			</div>
+		)
+	}
+
+	tick() {
+		this.setState({
+			time: new Date()
+		});
+	}
+
+	componentDidMount() {
+		this.timerID = setInterval( () => this.tick(), 1000);
+	}
+
+	componentWillUnmount() {
+		clearInterval(this.timerID);
+
+	}
+
+}
+
+ReatDOM.render(<Clock/>, document.getElementById('root'));
