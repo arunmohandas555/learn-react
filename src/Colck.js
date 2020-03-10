00ed838edd4520992b5d47f93520bcc295fb285a
arunmohandas <arun.mohandas@highpeaksw.com>
2020-03-10 12:21:54 +0530

    Moved Clock component to seperate js file

---
+++ src/Colck.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+class Clock extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {time: new Date()};
+	}
+
+	render() {
+		return (
+			<div>
+                This is the Clock Component <br/>
+				Time now is :  {this.state.time.toLocaleTimeString()}
+			</div>
+		)
+	}
+
+	tick() {
+		this.setState({
+			time: new Date()
+		});
+	}
+
+	componentDidMount() {
+		this.timerID = setInterval( () => this.tick(), 1000);
+	}
+
+	componentWillUnmount() {
+		clearInterval(this.timerID);
+
+	}
+
+}
+
+export default Clock
--- src/index.js
+++ src/index.js
@@ -2,36 +2,10 @@ import React from 'react';
 import ReatDOM from 'react-dom';
 import './index.css';
 
-class Clock extends React.Component {
-
-	constructor(props) {
-		super(props);
-		this.state = {time: new Date()};
-	}
-
 	render() {
-		return (
 			<div>
-				Time now is :  {this.state.time.toLocaleTimeString()}
 			</div>
-		)
 	}
-
-	tick() {
-		this.setState({
-			time: new Date()
-		});
-	}
-
-	componentDidMount() {
-		this.timerID = setInterval( () => this.tick(), 1000);
-	}
-
-	componentWillUnmount() {
-		clearInterval(this.timerID);
-
-	}
-
 }
 
-ReatDOM.render(<Clock/>, document.getElementById('root'));
+ReatDOM.render(<Clock/>, document.getElementById('root'));ReatDOM.render(<App/>, document.getElementById('root'));
