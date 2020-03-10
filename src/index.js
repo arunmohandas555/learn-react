351c54b49637c117f594d5fb1ad8a13b2f4d553c
arunmohandas <arun.mohandas@highpeaksw.com>
2020-03-10 12:22:44 +0530

    Added App as the main component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReatDOM from 'react-dom';
 import './index.css';
+import Clock from './Colck'
 
+class App extends React.Component {
+	
 	render() {
+		return(
 			<div>
+				This is the App Component
+				<Clock/>
 			</div>
+		);
 	}
 }
 
-ReatDOM.render(<Clock/>, document.getElementById('root'));ReatDOM.render(<App/>, document.getElementById('root'));
+ReatDOM.render(<App/>, document.getElementById('root'));
